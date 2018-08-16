97527e45755001238bfe1602df4ffb543e081028
Jhon Arias <jhonarmont@hotmail.com>
2018-08-16 18:55:14 -0500

    Created Questions controller and its Index view, this will be our home page which contains all the questions. In addition, we added a gem to integrate bootstrap to the project and we added a navbar just to verify it's working properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+  root 'questions#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
